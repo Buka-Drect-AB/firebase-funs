7cae783b08600a9a32df7799655f55988af7277d
cjackmay <confidence@jackmay.org>
2025-08-19 12:20:54 +0100

    updated

--- build/utils/index.d.ts
+++ build/utils/index.d.ts
@@ -3,3 +3,4 @@
  * @return {number} timestamp
  */
 export declare function unixTimeStampNow(): number;
+export declare function normalizeDate(input: number | Date | string | null | undefined): Date;
--- build/utils/index.js
+++ build/utils/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.unixTimeStampNow = unixTimeStampNow;
+exports.normalizeDate = normalizeDate;
 /**
  * get unix timestamp of now
  * @return {number} timestamp
@@ -8,5 +9,14 @@ exports.unixTimeStampNow = unixTimeStampNow;
 function unixTimeStampNow() {
     const now = new Date();
     return Math.floor(now.getTime() / 1000);
+}
+function normalizeDate(input) {
+    if (!input)
+        return new Date(0); // fail-safe
+    if (typeof input === 'number')
+        return new Date(input * 1000);
+    if (typeof input === 'string')
+        return new Date(input);
+    return input;
 }
 //# sourceMappingURL=index.js.map
